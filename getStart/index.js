97cf949e6b4aa4d808f3098cd2fc39d3ac6eee8f
Paulo Henrique da Costa Barros <paulo_costinha@hotmail.com>
2016-07-30 15:54:49 -0300

    Hello World - Exemplos do guide (até Methods e events)

---
+++ getStart/index.js
@@ -0,0 +1,68 @@
+new Vue({
+    el: '#helloWorld',
+    data: {
+        msg: 'Hello World VueJS!!',
+        a: 1
+    },
+    computed: {
+        b: function () {
+            return +this.a + 1;
+        }
+    },
+    methods: {
+        dizerOi: function (event) {
+            console.log('event', event);
+            alert('Olá');
+        }
+    }
+});
+
+new Vue({
+    el: '#form1',
+    data: {
+        nome: 'paulo barros',
+        options: [
+            {
+                color: 'Azul',
+                checked: true
+            },
+            {
+                color: 'Amarelo',
+                checked: false
+            },
+            {
+                color: 'Verde',
+                checked: true
+            },
+            {
+                color: 'Vermelho',
+                checked: false
+            }
+        ],
+        exibirForm: true,
+        user: {
+            name: 'Maria',
+            age: 10
+        }
+    },
+    computed: {
+        textButton: function() {
+            return this.exibirForm ? 'Esconder' : 'Exibir';
+        }
+    },
+    methods: {
+        selecionarCor: function (option, event) {
+            console.log('Você selecionou a cor: ' + option.color);
+            console.log(option);
+        },
+        esconderForm: function(){
+            this.exibirForm = !this.exibirForm;
+        },
+        abrirGoogle: function(){
+            console.log('google');
+        }
+    },
+    created: function () {
+        console.log('Created!');
+    }
+});
